c63c5d76a44ce7b564599f6faa5aa58302569d03
Pavel Palagin <95273237+pavel-palagin@users.noreply.github.com>
2024-01-11 21:56:39 +0300

    hw seminar_1 node.js

--- sem_01/seminar.js
+++ sem_01/seminar.js
@@ -1,21 +1,26 @@
 const http = require("http");
-const { listenerCount } = require("process");
 
 const server = http.createServer((req, res) => {
-  let count = 0;
+  //   function counter() {
+  //     document.addEventListener("DOMContentLoaded", () => {
+  //       let count = Number(localStorage.count);
+  //       localStorage.count = ++count;
+  //       console.log(count);
+  //     });
+  //   }
 
   if (req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/html; charset=UTF-8" });
     res.end(`
     <h1 class="title">Корневая страница</h1>
-    <p class="text">Просмотров: ${count}</p>
+    <p class="text">Просмотров: </p>  
     <a class="link" href="/about">About</a>
     `);
   } else if (req.url === "/about") {
     res.writeHead(200, { "Content-Type": "text/html; charset=UTF-8" });
     res.end(`
     <h1 class="title">Страница about</h1>
-    <p class="text">Просмотров: ${count}</p>
+    <p class="text">Просмотров: </p>
     <a class="link" href="/">На главную</a>
     `);
   } else {
